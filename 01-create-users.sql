0daf6cdb39346be0cf8c8d6edf3e434c2b324ea0
Alex <alex.hertel87@gmail.com>
2021-04-14 10:22:18 -0500

    finished project

--- 01-create-users.sql
+++ 01-create-users.sql
@@ -7,9 +7,20 @@
 -- | project_management_app | Q3HphPJ8 |
 
 
+CREATE USER aa_times
+WITH
+PASSWORD '6g73WE2V';
 
+CREATE USER project_management_app
+WITH
+PASSWORD 'Q3HphPJ8';
 
 -- Create the following superuser with the indicated name and password
 -- | name       | password |
 -- |------------|----------|
 -- | data_admin | ik2fiDri |
+
+CREATE USER data_admin
+WITH
+PASSWORD 'ik2fiDri'
+SUPERUSER;
--- 03-create-aa-times-tables.sql
+++ 03-create-aa-times-tables.sql
@@ -3,6 +3,9 @@
 -- Set the role to the aa_times user so the tables will be owned by that user
 SET ROLE aa_times;
 
+DROP TABLE IF EXISTS people;
+DROP TABLE IF EXISTS sections;
+DROP TABLE IF EXISTS stories;
 
 -- The "people" table
 -- | Column name | Type         | Constraints |
@@ -12,15 +15,25 @@ SET ROLE aa_times;
 -- | last_name   | VARCHAR(50)  | NOT NULL    |
 -- | email       | VARCHAR(100) | NOT NULL    |
 
+create table people
+(
+   id SERIAL PRIMARY KEY
+   , first_name VARCHAR(50) NOT NULL
+   , last_name VARCHAR(50) NOT NULL
+   , email VARCHAR(100) NOT NULL
+);
 
- 
 -- The "sections" table
 -- | Column name | Type         | Constraints |
 -- |-------------|--------------|-------------|
 -- | id          | SERIAL       | PRIMARY KEY |
 -- | name        | VARCHAR(150) | NOT NULL    |
-
-
+ 
+create table sections
+(
+   id SERIAL PRIMARY KEY
+   , name VARCHAR(150) NOT NULL
+);
 
 -- The "stories" table
 -- | Column name | Type    | Constraints                             |
@@ -29,3 +42,11 @@ SET ROLE aa_times;
 -- | author_id   | INTEGER | FOREIGN KEY to people table, NOT NULL   |
 -- | content     | TEXT    | NOT NULL                                |
 -- | section_id  | INTEGER | FOREIGN KEY to sections table, NOT NULL |
+
+CREATE TABLE stories
+(
+     id serial primary key
+    , author_id INTEGER references people (id)
+    , content TEXT
+    , section_id INTEGER references sections (id)
+);
